2f34763a2f88e4248e0f3f37024fb5862461c7b9
lkvlkvlkv <a23202418@gmail.com>
2025-03-24 20:35:28 +0800

    feat: add statistics generation and summary CSV output for test results

---
+++ statistics.py
@@ -0,0 +1,31 @@
+import pandas as pd
+
+import os
+
+base_dir = './result/'
+dates = ['best', '0217', '0319']
+test_dirs = ['Brandimarte_Data', \
+             'data_dev/1005', 'data_dev/1510', 'data_dev/2005', 'data_dev/2010', \
+             'Hurink_Data/rdata', 'Hurink_Data/edata', 'Hurink_Data/vdata',]
+
+
+for date in dates:
+    data_frame = pd.DataFrame(columns=['instance', 'avg_makespan', 'avg_time', 'avg_terminate', 'terminate_ratio', 'avg_q_time_penalty'])
+
+    for test_dir in test_dirs:
+        file_path = os.path.join(base_dir, date, test_dir)
+        
+        if os.path.exists(file_path):
+            df = pd.read_csv(os.path.join(file_path, 'test_result.csv'))
+            avg_makespan = df['makespan'].mean()
+            avg_time = df['time'].mean()
+            terminate_count = df['terminate'].sum()
+            terminate_ratio = 100 * terminate_count / len(df)
+            avg_q_time_penalty = df['exceed_q_time'].mean()
+
+            instance = test_dir.split('/')[-1]
+            new_row = pd.DataFrame([{'instance': instance, 'avg_makespan': avg_makespan, 'avg_time': avg_time, 'avg_terminate': terminate_count, 'terminate_ratio': terminate_ratio, 'avg_q_time_penalty': avg_q_time_penalty}])
+            data_frame = pd.concat([data_frame, new_row], ignore_index=True)
+
+    data_frame.to_csv(os.path.join(base_dir, date, 'summary.csv'), index=False)
+    print(f"Summary for date {date} saved.")
--- test.py
+++ test.py
@@ -4,27 +4,41 @@ from env.env import JSP_Env
 from model.REINFORCE import REINFORCE
 import time
 import os
+import pandas as pd
 
 def test():
+    data_frame = pd.DataFrame(columns=['instance', 'makespan', 'time', 'terminate', 'exceed_q_time'])
+    
     for instance in os.listdir(args.test_dir):
         file = os.path.join(args.test_dir, instance)
         avai_ops = env.load_instance(file)
         st = time.time()
+        q_time_limit_exceed = False
 
         while True:
             data, op_unfinished= env.get_graph_data()
             action_idx, action_prob = policy(avai_ops, data, op_unfinished, env.jsp_instance.graph.max_process_time, greedy=True)
-            avai_ops, _, done, _ = env.step(avai_ops[action_idx])
+            avai_ops, _, done, terminate, exceed_q_time = env.step(avai_ops[action_idx])
+
+            if terminate:
+                q_time_limit_exceed = True
             
             if done:
                 ed = time.time()
                 policy.clear_memory()
 
-                print("instance : {}, ms : {}, time : {}".format(file, env.get_makespan(), ed - st))
+                new_row = pd.DataFrame([{'instance': file, 'makespan': env.get_makespan(), 'time': ed - st, 'terminate': 0 if q_time_limit_exceed else 1, 'exceed_q_time': exceed_q_time}])
+                data_frame = pd.concat([data_frame, new_row], ignore_index=True)
+                print("instance : {}, ms : {}, time : {}, q_time_limit_exceed : {}, exceed_q_time : {}".format(file, env.get_makespan(), ed - st, q_time_limit_exceed, exceed_q_time))
+
                 with open("./result/{}/test_result.txt".format(args.date),"a") as outfile:
                     outfile.write(f'instance : {file:60}, policy : {env.get_makespan():10}\t')
-                    outfile.write(f'time : {ed - st:10}\n')
+                    outfile.write(f'time : {ed - st:10}\t terminate : {terminate}\n')
                 break
+    print(f'{args.date}, avg_ms : {data_frame["makespan"].mean()}, avg_time : {data_frame["time"].mean()}, terminate : {data_frame["terminate"].sum()}, terminate_ratio : {data_frame["terminate"].sum() / len(data_frame)}')
+    file_name = args.test_dir.replace('./datasets/FJSP/', '').replace('/Text', '')
+    os.makedirs(f"./result/{args.date}/{file_name}/", exist_ok=True)
+    data_frame.to_csv(f"./result/{args.date}/{file_name}/test_result.csv", index=False)
 
 if __name__ == '__main__':
     args = get_args()
